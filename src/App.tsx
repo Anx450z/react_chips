adf75942101b26739e8b9af260ac70563bcc0dd5
= <=>
2022-10-25 19:31:23 +0530

    add options and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
+import Select from './Components/Select'
+
+const options = [
+  {label : 'first', value: 1},
+  {label : 'second', value: 2},
+  {label : 'third', value: 3},
+  {label : 'forth', value: 4},
+
+]
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hi !</h1>
+    <Select options={options}/>
     </>
   )
 }
