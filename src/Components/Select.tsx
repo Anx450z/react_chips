9697f54e70193e783657ba44aad4760231d727d5
= <=>
2022-10-25 22:21:25 +0530

    completed css of elements

--- src/Components/Select.tsx
+++ src/Components/Select.tsx
@@ -19,7 +19,7 @@ const Select = ({ value, onChange, options }: SelectProps) => {
       <button className={styles["clear-btn"]}>&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul>
+      <ul className={`${styles.options} ${styles.show}`}>
         {options.map((option) => (
           <li key={option.label} className={styles.option}>
             {" "}
